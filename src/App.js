5b6f06ff02fcec206598c21ececc3c138cc03baa
coderkhushal <137866759+coderkhushal@users.noreply.github.com>
2024-02-09 23:57:53 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/footer";
 import Blogs from "./pages/Blogs/Blogs";
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <main className="overflow-x-auto">
+    <BrowserRouter>
       <Navbar />
-      <Blogs />
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/blogs" element={<Blogs/>}/>
+      
+      </Routes>
       <Footer />
+    </BrowserRouter>
     </main>
   );
 }
