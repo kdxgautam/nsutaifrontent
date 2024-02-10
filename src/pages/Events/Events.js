b8c22ae956656428cfa48964bd70a3f6e7acace6
kdxgautam <kdxgautam@gmail.com>
2024-02-10 17:54:42 +0530

    made event page responsive

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,24 +1,24 @@
-import EventCard from "./components/EventCard"
-import Footer from "../../components/footer"
-import Navbar from "../../components/Navbar"
+import EventCard from "./EventCard"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
 
 
 const EventFrame = () => {
   return (
     <>
       <Navbar />
-      <div className="flex w-full justify-center items-center mt-28 pt-[70px]">
-        <div className="text-center  w-[450px] min-w-[300px] flex flex-col translate-x-14">
-          <h1 className="text-[#BBBCC0] text-5xl ">
+      <div className="flex w-full justify-center items-center mt-28 pt-[70px] ">
+        <div className="text-center  w-[450px] min-w-[300px] flex flex-col pl-7">
+          <h1 className="text-[#BBBCC0] text-5xl lg:text-7xl">
             Lorem ipsum dolor, sit amet
           </h1>
-          <h6 className="text-[#BBBCC0]">Lorem ipsum dolor, sit amet</h6>
+          <h6 className="text-[#BBBCC0] mt-4">Lorem ipsum dolor, sit amet</h6>
         </div>
 
-        <div className="w-1/2 items-center flex justify-center h-3/4 -translate-x-14">
-          <img className="relative top-[-200px] right-[-120px] max-w-full" src="/images/vector.png" alt="" />
-          <img className="max-w-full" src="/images/robot.png" alt="" />
-          <img className="max-w-full relative top-[100px] right-[]" src="/images/vector2.png" alt="" />
+        <div className="w-1/2 items-center flex justify-center h-3/4 pr-[2vw]">
+          <img className="absolute opacity-0 md:opacity-100 md:relative  md:top-[-15vw] md:right-[-120px] max-w-full " src="/images/vector.png" alt="vector" />
+          <img className="max-w-full " src="/images/robot.png" alt="robot" />
+          <img className="absolute opacity-0 md:opacity-100 md:relative  md:top-[100px] max-w-full" src="/images/vector2.png" alt="vector" />
         </div>
         
       </div>
