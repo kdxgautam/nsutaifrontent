a3248ffdda8ce9db542d397fec220e06cc46b513
Aditya <aditya72.saini@gmail.com>
2024-02-25 17:45:30 +0530

    Responsive single event page

--- src/pages/SingleEvent/singleevent.js
+++ src/pages/SingleEvent/singleevent.js
@@ -1,59 +1,53 @@
 import Footer from "./Footer";
 import Navbar from "./NavBar";
-
 const SingleEvent = () => {
   return (
     <>
       <Navbar />
       <div>
         <img
-          className="h-[30rem] w-[100%]"
+          className="w-full h-auto max-h-[30rem]"
           src="https://cdn.wallpapersafari.com/14/40/pPnVIZ.jpg"
+          alt="Event Image"
         />
-        <div className=" relative bottom-20 text-3xl  flex justify-center items-end text-white">
+        <div className="relative bottom-10 sm:bottom-20 text-lg sm:text-xl sm:flex md:text-2xl lg:text-3xl xl:text-4xl flex justify-center items-end text-white">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
         </div>
       </div>
-      <div className="flex just text-white p-[5rem] pt-[4rem]">
-        <div className=" pl-20 pr-[30rem] text-justify leading-5 ">
-          <span className="text-4xl  ">Event Details</span>
+      <div className="flex flex-col sm:flex-row justify-center items-center text-white p-5 sm:px-10 lg:px-20 xl:px-40 pt-4 lg:ml-20">
+        <div className="text-justify ml-[1rem] leading-5 sm:mr-4">
+          <span className="text-4xl">Event Details</span>
           <br />
           <span className="text-xl leading-10">
-            Venue : Connecting Block
+            Venue: Connecting Block
             <br />
-            Time : 11am-3pm
+            Time: 11am-3pm
           </span>
-          <ul class="list-disc">
-            <li>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Praesentium dignissimos expedita omnis vero minima molestiae
-              suscipit assumenda enim. Dolores et optio, atque nam maxime libero
-              laborum sit provident neque perspiciatis.
-            </li>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-              earum veniam, recusandae sequi deserunt omnis quam reprehenderit
-              eaque molestiae saepe quis, nemo sit similique odio sint,
-              provident nostrum delectus! Voluptate?
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum totam
-              quisquam ea quibusdam fugit est ipsa excepturi quae, quod tenetur
-              quos similique reprehenderit odit minus, eveniet iure debitis
-              sequi velit.
-            </li>
+          <ul className="list-disc">
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit...</li>
+            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit...</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit...</li>
           </ul>
-          <button className="bg-[#415ED0] mx-1 text-white px-4 py-2 mt-6 rounded-full transition duration-200 ease-in-out hover:bg-blue-700  focus:outline-none">
+          <button className="bg-[#415ED0] mx-1 text-white px-4 py-2 mt-6 rounded-full transition duration-200 ease-in-out hover:bg-blue-700 focus:outline-none">
             Register Here
           </button>
         </div>
-        <div className="relative right-[15rem] bg-[#202127] h-[4rem] w-[70rem] mt-20  rounded-xl  ">
+        <div className="sm:hidden mt-4">
+          <span>Registration Ends In:</span>
+          <br />
+          <span className="text-xl">06:55:35</span>
+        </div>
+        <div className="hidden sm:block relative h-[4rem] w-[15rem] mt-4 sm:mt-3 ml-0 sm:ml-4 bg-[#202127] lg:ml-8 bottom-[8rem] right-[4.5rem] rounded-xl justify-center">
           <span className="p-5">Registration Ends In:</span>
           <br />
-          <span className="pl-[3.5rem] text-xl">06:55:35</span>
+          <span className="pl-[3rem] text-xl">06:55:35</span>
         </div>
-        <div className="relative right-[8rem]  top-[9rem]">
-          <img className="h-[25rem] w-[100rem]" src="images/robot.png" />
+        <div className="relative mt-4 sm:mt-0 sm:right-[8rem] lg:top-[1rem]">
+          <img
+            className="w-full h-auto max-h-[25rem] sm:max-w-[35rem]"
+            src="images/robot.png"
+            alt="Robot Image"
+          />
         </div>
       </div>
       <Footer />
