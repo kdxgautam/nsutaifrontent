d785816e2e2a8ee176060c557b9b2b1082fd90bd
navtesh21 <makennavtesh@gmail.com>
2024-02-29 13:42:28 +0530

    profile page

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -4,65 +4,101 @@ import DepartmentsMob from "./components/DepartmentsMob";
 import { useEffect, useState } from "react";
 const host = "http://127.0.0.1:4000";
 
-
-  
-  
 const AboutPage = () => {
-    const [profile, setProfile] = useState([]);
- 
-
-
-
+  const [profile, setProfile] = useState([]);
 
   const fetchProfile = async () => {
-    let res = await fetch(host + "/profile/all");
+    let res = await fetch(host + "/members/all");
     let resdata = await res.json();
     setProfile(resdata);
   };
   useEffect(() => {
     fetchProfile();
   }, []);
-    return (
-        <>
-            <main className="overflow-hidden">
-                <div className="text-center pt-44 pb-12">
-                    <h1 className="text-xl text-[#BBBCC0] font-inter font-normal md:max-xl:text-md">About NSUT.AI</h1>
-                </div>
-                <div className="flex justify-center items-center w-full ">
-                    <img src="/images/about.jpg" alt="aboutimage" className=" rounded-[4rem] h-full w-1/2  " />
-                </div>
-                <div className="pt-12 flex items-center justify-center">
-                    <p className="text-center text-[#BBBCC0] font-light text-[0.8rem] md:text-[1.2rem]">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br />
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br />
-                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br />
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu<br />
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in<br />
-                        culpa qui officia deserunt mollit anim id est laborum.</p></div>
-                <div className="text-center pt-36 pb-12"><h1 className="text-xl text-[#BBBCC0] font-inter font-normal md:max-xl:text-md">Departments</h1></div>
-                <div className="md:flex items-center justify-center hidden "><DepartmentsList/></div>
-             <div className="md:flex items-center justify-center md:hidden md:max-xl:hidden xl:hidden "><DepartmentsMob/></div>
-                <div className="text-center pt-36 pb-12"><h1 className="text-xl text-[#BBBCC0] font-inter font-normal md:max-xl:text-md">Core Members</h1></div>
-             <div className="flex justify-center items-center gap-2 xl:px-[33rem] font-light md:max-xl:text-sm px-[2rem] md:max-xl:px-[2rem] pb-[4rem] ">
-                    <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[7rem] xl:w-[16rem] xl:h-[1.8rem] text-center pt-1">Software Department</div>
-                    <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[7rem] xl:w-[16rem] xl:h-[1.8rem] text-center pt-1">Research Department</div>
-                    <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[3rem] xl:w-[4rem]  xl:h-[1.8rem] text-center pt-1">All</div>
-                    <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[7rem] xl:w-[16rem] xl:h-[1.8rem] text-center pt-1">AI&ML Department</div>
-                    <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[7rem] xl:w-[16rem] xl:h-[1.8rem] text-center pt-1">Graphics Department</div>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-            {profile.length!==0 ? profile.map((event)=>(<ProfileCard key={profile._id} id={profile._id} name = {event.name} image={event.image} />)) : <h1 className="text-white">No profile found</h1> }
-                
-            </div>
-            <div className="pt-44">
-                </div>
-
-            </main>
-
-        </>
-
-
-    )
-}
+  return (
+    <>
+      <main className="overflow-hidden">
+        <div className="text-center pt-44 pb-12">
+          <h1 className="text-xl text-[#BBBCC0] font-inter font-normal md:max-xl:text-md">
+            About NSUT.AI
+          </h1>
+        </div>
+        <div className="flex justify-center items-center w-full ">
+          <img
+            src="/images/about.jpg"
+            alt="aboutimage"
+            className=" rounded-[4rem] h-full w-1/2  "
+          />
+        </div>
+        <div className="pt-12 flex items-center justify-center">
+          <p className="text-center text-[#BBBCC0] font-light text-[0.8rem] md:text-[1.2rem]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor <br />
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis <br />
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+            <br />
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu
+            <br />
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in
+            <br />
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="text-center pt-36 pb-12">
+          <h1 className="text-xl text-[#BBBCC0] font-inter font-normal md:max-xl:text-md">
+            Departments
+          </h1>
+        </div>
+        <div className="md:flex items-center justify-center hidden ">
+          <DepartmentsList />
+        </div>
+        <div className="md:flex items-center justify-center md:hidden md:max-xl:hidden xl:hidden ">
+          <DepartmentsMob />
+        </div>
+        <div className="text-center pt-36 pb-12">
+          <h1 className="text-xl text-[#BBBCC0] font-inter font-normal md:max-xl:text-md">
+            Core Members
+          </h1>
+        </div>
+        <div className="flex justify-center items-center gap-2 xl:px-[33rem] font-light md:max-xl:text-sm px-[2rem] md:max-xl:px-[2rem] pb-[4rem] ">
+          <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[7rem] xl:w-[16rem] xl:h-[1.8rem] text-center pt-1">
+            Software Department
+          </div>
+          <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[7rem] xl:w-[16rem] xl:h-[1.8rem] text-center pt-1">
+            Research Department
+          </div>
+          <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[3rem] xl:w-[4rem]  xl:h-[1.8rem] text-center pt-1">
+            All
+          </div>
+          <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[7rem] xl:w-[16rem] xl:h-[1.8rem] text-center pt-1">
+            AI&ML Department
+          </div>
+          <div className="hover:bg-[#415ED0] cursor-pointer rounded-2xl text-[0.5rem] sm:text-[0.8rem] w-[7rem] xl:w-[16rem] xl:h-[1.8rem] text-center pt-1">
+            Graphics Department
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          {profile.length !== 0 ? (
+            profile.map((event) => (
+              <ProfileCard
+                key={profile._id}
+                id={event._id}
+                name={event.name}
+                image={event.image}
+              />
+            ))
+          ) : (
+            <h1 className="text-white">No profile found</h1>
+          )}
+        </div>
+        <div className="pt-44"></div>
+      </main>
+    </>
+  );
+};
 
 export default AboutPage;
--- src/pages/About/ProfilePage.js
+++ src/pages/About/ProfilePage.js
@@ -1,49 +1,109 @@
-import Footer from "../../components/footer"
-import ProfileCard from "./components/ProfileCard"
+import Footer from "../../components/footer";
+import ProfileCard from "./components/ProfileCard";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
+const host = "http://localhost:4000";
 
 const ProfilePage = () => {
-    return (
-        <>
-        <main>
-         
-            <div className="flex justify-center items-center pt-[4rem]"><ProfileCard/></div>
-            <div className="flex items-center justify-center ">  
-            <div className="flex-col justify-center items-center xl:w-[80rem] xl:h-[40rem] w-[30rem] bg-[#121216] rounded-[4rem]">
-                               
-                <h2 className="text-white pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[4rem]">Department:</h2>
-                <h4 className="font-light pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[1rem] opacity-70">Software</h4>
+  const { id } = useParams();
+  console.log(id);
+  const [profile, setProfile] = useState();
 
-                <h2 className="text-white pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[2rem] ">Achievements:</h2>
-                <h4 className="font-light  pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[1rem] opacity-70 align-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                    cididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis 
-                     nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                     Duis aute</h4>
+  const fetchthings = async () => {
+    const token = localStorage.getItem("AdminToken");
+    if (!token) {
+      alert("Not authorised");
+      return;
+    }
 
-                <h2 className="text-white pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[4rem] ">Skills:</h2>
-                <div className=" pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[1rem]">
-                    <button className="bg-[#415ED0] mx-1 border text-white px-4 py-2 rounded-full">React</button>
-                    <button className="bg-[#415ED0] mx-1 border text-white px-4 py-2 rounded-full">Javascript</button>
-                    <button className="bg-[#415ED0] mx-1 border text-white px-4 py-2 rounded-full">Tailwind</button>
-                    <button className="bg-[#415ED0] mx-1 border text-white px-4 py-2 rounded-full">Figma</button>
-                </div>
+    const res = await fetch(`${host}/members/${id}`, {
+      method: "GET",
+      headers: {
+        token: token,
+      },
+    });
+    const resdata = await res.json();
+    console.log(resdata);
+    setProfile(resdata);
+    console.log(profile);
+  };
 
-                <h2 className="text-white pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[4rem]">Connect at:</h2>
-                <div className="pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[1rem] pb-[2rem] flex">
-                    <img src="/images/github.png" alt="" className="w-[3rem]"></img>
-                    <img src="/images/linkedin.png" alt=""></img>
-                    </div>
-                </div>
+  useEffect(() => {
+    fetchthings();
+  }, []);
 
+  return (
+    <>
+      <main>
+        <div className="flex justify-center items-center pt-[4rem]">
+          <ProfileCard
+            id={profile._id}
+            name={profile.name}
+            image={profile.image}
+          />
+        </div>
+        <div className="flex items-center justify-center ">
+          <div className="flex-col justify-center items-center xl:w-[80rem] xl:h-[40rem] w-[30rem] bg-[#121216] rounded-[4rem]">
+            <h2 className="text-white pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[4rem]">
+              Department:
+            </h2>
+            {profile.department.map((e) => {
+              <h4 className="font-light pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[1rem] opacity-70">
+                e
+              </h4>;
+            })}
+            <h2 className="text-white pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[2rem] ">
+              Achievements:
+            </h2>
+            {profile.achievements.map((e) => {
+              <h4 className="font-light  pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[1rem] opacity-70 align-left">
+                e
+              </h4>;
+            })}
 
-                
-                </div>
-                
-            <div className="pt-52"><Footer/></div>
-        </main>
-        </>
-    )
-}
+            <h2 className="text-white pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[4rem] ">
+              Skills:
+            </h2>
+            <div className=" pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[1rem]">
+              <button className="bg-[#415ED0] mx-1 border text-white px-4 py-2 rounded-full">
+                React
+              </button>
+              <button className="bg-[#415ED0] mx-1 border text-white px-4 py-2 rounded-full">
+                Javascript
+              </button>
+              <button className="bg-[#415ED0] mx-1 border text-white px-4 py-2 rounded-full">
+                Tailwind
+              </button>
+              <button className="bg-[#415ED0] mx-1 border text-white px-4 py-2 rounded-full">
+                Figma
+              </button>
+            </div>
 
-export default ProfilePage
+            <h2 className="text-white pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[4rem]">
+              Connect at:
+            </h2>
+            <div className="pl-[4rem] pt-[2rem] xl:pl-[6rem] xl:pt-[1rem] pb-[2rem] flex">
+              <a href={profile.social.github} target="_blank">
+                <img
+                  src="/images/github.png"
+                  alt="github link"
+                  className="w-[3rem]"
+                ></img>
+              </a>
+              <a href={profile.social.linkedin} target="_blank">
+                <img src="/images/linkedin.png" alt="kinkedin link"></img>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="pt-52">
+          <Footer />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default ProfilePage;
